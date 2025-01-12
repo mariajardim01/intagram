f68c8e3f0c5fd2668f73690348b3600a7d0dc63c
maria <mahjardim02@gmail.com>
2025-01-12 12:59:15 -0300

    feat: added dinamic user name

--- src/components/User.js
+++ src/components/User.js
@@ -1,11 +1,25 @@
+import React, { useState } from "react"
+
 export default function User(){
+
+const [name,setName] = React.useState("Maria")
+
+function changeName(){
+    let newName = prompt("Qual nome você deseja inserir?")
+    if (!newName){}
+    else{
+        setName(newName)
+    }
+}
+
+
     return(<>
     <div class="usuario">
           <img src="assets/img/catanacomics.svg" alt="imagem de perfil"/>
           <div class="texto">
             <span>
-              <strong>catanacomics</strong>
-              <ion-icon name="pencil"></ion-icon>
+              <strong>{name}</strong>
+              <ion-icon name="pencil" onClick={()=>changeName()}></ion-icon>
             </span>
           </div>
     </div></>)
