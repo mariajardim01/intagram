123cc48db2d9199cbd540b0315556d14967bbc2e
maria <mahjardim02@gmail.com>
2025-01-12 13:04:09 -0300

    feat: added dinamic user image

--- src/components/User.js
+++ src/components/User.js
@@ -3,6 +3,7 @@ import React, { useState } from "react"
 export default function User(){
 
 const [name,setName] = React.useState("Maria")
+const [image,setImage] = React.useState("assets/img/catanacomics.svg")
 
 function changeName(){
     let newName = prompt("Qual nome você deseja inserir?")
@@ -12,10 +13,19 @@ function changeName(){
     }
 }
 
+function changeImage(){
+    let newImage = prompt("Insira o link da nova imagem:")
+    if (!newImage){}
+    else {
+        setImage(newImage)
+    }
+
+}
+
 
     return(<>
     <div class="usuario">
-          <img src="assets/img/catanacomics.svg" alt="imagem de perfil"/>
+          <img src={image} alt="imagem de perfil" onClick={()=>changeImage()}/>
           <div class="texto">
             <span>
               <strong>{name}</strong>
